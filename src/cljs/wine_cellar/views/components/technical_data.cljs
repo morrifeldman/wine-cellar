852a32424223c1f7e4fef132ed7b901e49add85f
morri <morrifeldman@gmail.com>
2025-12-20 13:05:42 -0500

    Make technical notes editable

--- src/cljs/wine_cellar/views/components/technical_data.cljs
+++ src/cljs/wine_cellar/views/components/technical_data.cljs
@@ -5,6 +5,10 @@
             [reagent-mui.material.box :refer [box]]
             [reagent-mui.material.text-field :refer [text-field]]
             [reagent-mui.material.icon-button :refer [icon-button]]
+            [reagent-mui.icons.edit :refer [edit]]
+            [reagent-mui.icons.save :refer [save]]
+            [reagent-mui.icons.cancel :refer [cancel]]
+            [reagent-mui.icons.delete :refer [delete]]
             [reagent-mui.material.typography :refer [typography]]
             [reagent-mui.material.autocomplete :refer [autocomplete]]
             [reagent-mui.material.button :refer [button]]
@@ -20,6 +24,45 @@
       str/lower-case
       (str/replace #"\s+" "-")
       keyword))
+
+(defn inline-editor
+  [{:keys [value on-save on-delete]}]
+  (r/with-let
+   [editing? (r/atom false) temp-value (r/atom value)]
+   (if @editing?
+     ;; Edit Mode
+     [box {:display "flex" :alignItems "flex-start" :width "100%"}
+      [text-field
+       {:full-width true
+        :variant "outlined"
+        :size "small"
+        :multiline true
+        :value @temp-value
+        :auto-focus true
+        :on-change #(reset! temp-value (.. % -target -value))}]
+      [box {:display "flex" :flexDirection "column" :ml 0.5}
+       [icon-button
+        {:color "primary"
+         :size "small"
+         :on-click #(do (on-save @temp-value) (reset! editing? false))} [save]]
+       [icon-button
+        {:color "secondary"
+         :size "small"
+         :on-click #(do (reset! temp-value value) (reset! editing? false))}
+        [cancel]]]]
+     ;; View Mode
+     [box
+      {:display "flex"
+       :alignItems "center"
+       :justifyContent "space-between"
+       :width "100%"}
+      [typography {:variant "body2" :sx {:flex 1 :mr 1}} (str value)]
+      [box {:display "flex" :gap 0.5}
+       [icon-button
+        {:color "primary" :size "small" :on-click #(reset! editing? true)}
+        [edit]]
+       [icon-button {:color "error" :size "small" :on-click on-delete}
+        [delete]]]])))
 
 (defn technical-data-editor
   "Component to edit arbitrary metadata fields.
@@ -44,6 +87,7 @@
                        (js->clj metadata :keywordize-keys true)
                        metadata)
             handle-delete (fn [k] (on-change (dissoc metadata k)))
+            handle-save (fn [k v] (on-change (assoc metadata k v)))
             handle-add (fn []
                          (let [k (normalize-input-key @new-key-input)
                                v @new-value]
@@ -69,11 +113,11 @@
                  {:variant "body2"
                   :sx {:fontWeight "bold" :color "text.secondary"}}
                  (common/humanize-key k)]]
-               [grid {:item true :xs 7} [typography {:variant "body2"} (str v)]]
-               [grid {:item true :xs 1}
-                [icon-button
-                 {:size "small" :color "error" :on-click #(handle-delete k)}
-                 "×"]]])])
+               [grid {:item true :xs 8}
+                [inline-editor
+                 {:value v
+                  :on-save #(handle-save k %)
+                  :on-delete #(handle-delete k)}]]])])
          ;; Add New Entry
          [grid {:container true :spacing 2 :sx {:alignItems "flex-start"}}
           [grid {:item true :xs 4}
