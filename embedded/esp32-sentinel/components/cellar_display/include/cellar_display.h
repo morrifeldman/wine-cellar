8c6dd7464120e7e5bd9b3e6e0cffc4186a4efb47
morri <morrifeldman@gmail.com>
2026-02-12 19:28:10 -0500

    fix off by one in DS display

--- embedded/esp32-sentinel/components/cellar_display/include/cellar_display.h
+++ embedded/esp32-sentinel/components/cellar_display/include/cellar_display.h
@@ -6,7 +6,7 @@
 #include "esp_err.h"
 
 #define CELLAR_DISPLAY_MAX_TEMPS 6
-#define CELLAR_DISPLAY_LABEL_LEN 16
+#define CELLAR_DISPLAY_LABEL_LEN 17
 
 typedef struct {
     float temps[CELLAR_DISPLAY_MAX_TEMPS];
